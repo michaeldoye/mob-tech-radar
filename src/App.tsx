186bef76632ec0b7a0578f411930d27840d09d6f
Michael Doye <mdoye@mobiquityinc.com>
2019-04-16 10:12:34 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {radarConfig} from "./radar.config";
-
-interface RadarConfig {
-  svg_id: string;
-  width: number;
-  height: number;
-  colors: RadarColor;
-  title: string;
-  quadrants: RadarQuadrants[];
-  rings: RadarRing[];
-  print_layout: boolean;
-  entries: RadarEntries[];
-}
-
-interface RadarQuadrants {
-  name: string;
-}
-
-interface RadarRing {
-  name: string;
-  color: string;
-}
-
-interface RadarColor {
-  background: string;
-  grid: string;
-  inactive: string;
-}
-
-interface RadarEntries {
-  label: string;
-  quadrant: number;
-  ring: number;
-  moved: number;
-  link?: string;
-  active?: boolean;
-}
+import { RadarConfig, radarConfig } from "./radar.config";
 
 declare function radar_visualization(config: RadarConfig): void;
 
--- src/radar.config.ts
+++ src/radar.config.ts
@@ -1,4 +1,39 @@
-import {RadarConfig} from "./radar";
+export interface RadarConfig {
+  svg_id: string;
+  width: number;
+  height: number;
+  colors: RadarColor;
+  title: string;
+  quadrants: RadarQuadrants[];
+  rings: RadarRing[];
+  print_layout: boolean;
+  entries: RadarEntries[];
+}
+
+interface RadarQuadrants {
+  name: string;
+}
+
+interface RadarRing {
+  name: string;
+  color: string;
+}
+
+interface RadarColor {
+  background: string;
+  grid: string;
+  inactive: string;
+}
+
+interface RadarEntries {
+  label: string;
+  quadrant: number;
+  ring: number;
+  moved: number;
+  link?: string;
+  active?: boolean;
+}
+
 
 export const radarConfig: RadarConfig = {
   svg_id: "radar",
