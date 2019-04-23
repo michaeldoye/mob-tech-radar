8b95ad83ba93fe81ef1cbcb1d228cc6db4a9a054
Mario Peusschers <nightmedow@gmail.com>
2019-04-23 16:33:05 +0200

    Updates

---
+++ precache-manifest.da079398e80026f4e86cb14da1c9e2b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "f2cdb7a9713a090b7410",
+    "url": "/my-app/static/js/main.2b8323e6.chunk.js"
+  },
+  {
+    "revision": "38a9481542ad04c4297a",
+    "url": "/my-app/static/js/2.b469ffca.chunk.js"
+  },
+  {
+    "revision": "f2cdb7a9713a090b7410",
+    "url": "/my-app/static/css/main.9b4b2273.chunk.css"
+  },
+  {
+    "revision": "cc8edc94ed795a7977ea7b900b494c4e",
+    "url": "/my-app/index.html"
+  }
+];
