65880eee7f5cba4da5f8c6f832623f3833abdd0d
Mario Peusschers <nightmedow@gmail.com>
2019-04-24 11:35:28 +0200

    Updates

---
+++ precache-manifest.32c125d4bff50a4b20e6e1104a056808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d2af52204966cd718558",
+    "url": "/static/js/main.dec2bb0d.chunk.js"
+  },
+  {
+    "revision": "38a9481542ad04c4297a",
+    "url": "/static/js/2.b469ffca.chunk.js"
+  },
+  {
+    "revision": "d2af52204966cd718558",
+    "url": "/static/css/main.9b4b2273.chunk.css"
+  },
+  {
+    "revision": "d58c2aef2a525eca1ff64ba5d08a3625",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mob-tech-radar/precache-manifest.24011a962c12d693772810611307a61c.js"
+  "/precache-manifest.32c125d4bff50a4b20e6e1104a056808.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/mob-tech-radar/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
